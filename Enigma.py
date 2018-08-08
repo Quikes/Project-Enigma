700bff4d5ab11d0541fbdb0595dc1b6ae20b7e74
Quikes <Quikeskun@gmail.com>
2018-08-08 18:30:29 +0200

    Troche zmian na klasy, skok

--- Enigma.py
+++ Enigma.py
@@ -5,33 +5,68 @@ import random
 import sys
 
 from Functions.simple_functions import *
-from Functions.Menu import menu_loop
 
+from Functions.Menu import menu_loop
+from Functions import Menu
+
+    
+
+""" pygame.key.set_repeat (50, 30) """  
 pygame.init()
 pygame.display.set_caption('Project Enigma')
+pygame.HWSURFACE|pygame.DOUBLEBUF
 clock=pygame.time.Clock() 
 
 def game_loop():
     
+    main_character=Menu.mch()
+    player = Player(400,500,main_character)
+    blocky = Platform(600,400,200,100,black)
     menu = False
     GameExit = False
-    while not GameExit:
+    while not GameExit: 
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            player.move(event)
+           
+        
         #message_display(text,x,y,color,font='freesansbold.ttf',size=30) 
 
-        
+        player.update()
         gameDisplay.fill(blue)
+
         message_display("Hehe tu nic nie ma",(display_width*0.6),(display_height*0.5),green) 
         
-        gameDisplay.blit(main_char,(display_width*0.4,display_height*(0.05)))
+        
+        
+
+        
+        
+       
+
+        
+        count_fps()
+        player.draw()
+        blocky.draw()
+        
+        """ player.move(speedx,speedy) """
         pygame.display.update()
+
+
+
+
+        
                    
-        clock.tick(60)
+        clock.tick()
+
 menu_loop()
+
 game_loop()
+
+
 
 pygame.quit()
 quit()
--- Functions/Menu.py
+++ Functions/Menu.py
@@ -1,30 +1,53 @@
 import pygame,time,random
 from Functions.simple_functions import *
 
+
+
 pygame.init()
 pygame.mixer.music.play()
 pygame.mixer.music.stop()
-music=True
 
-main_character=pygame.transform.scale(main_character,(150,250))
+main_character = char_img0
+
+def mch():
+    global main_character
+    main_character = main_character
+    return main_character
+
+music=True
+def char1():
+    global main_character 
+    main_character = char_img0
+    main_char=main_character
+def char2():
+    global main_character 
+    main_character= char_img1
+    main_char=main_character
+def char3():
+    global main_character 
+    main_character=char_img2
+    
 clock=pygame.time.Clock() 
 def pick_char():
+    from Enigma import game_loop
     pick_char = True
-
+    
     while pick_char:
+         
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit_all()
             gameDisplay.fill(black)
             textfont=pygame.font.Font('freesansbold.ttf',30)
             textsurf, textrect = text_objects('Pick your hero',textfont,white)
-            textrect.center = ((display_width*0.5),(display_height*0.1))
+            textrect.center = ((display_width*0.7),(display_height*0.1))
             gameDisplay.blit(textsurf,textrect)
-            
             rectan_button('Back',display_width*0.35,display_height*0.8,200,100,green,green_bright,menu_loop)
             rectan_button('char1',display_width*0.2,display_height*0.5,100,50,green,green_bright,char1)
             rectan_button('char2',display_width*0.4,display_height*0.5,100,50,green,green_bright,char2)
             rectan_button('char3',display_width*0.6,display_height*0.5,100,50,green,green_bright,char3)
+            rectan_button('Game Start',display_width*0.6,display_height*0.8,200,100,green,green_bright,game_loop)
 
             gameDisplay.blit(main_character,(display_width*0.4,display_height*(0.05)))
             pygame.display.update()
@@ -51,7 +74,7 @@ def options_loop():
             clock.tick(15)
 def menu_loop():
     #bez tegp się zapętla czy cos jak najpierw importuje game_loop globalnie, bo w Enigmie uzyty jest menu_loop czy cos takiego idk ;/
-    from Enigma import game_loop 
+     
     options = False
     menu = True 
     while menu:
@@ -65,7 +88,7 @@ def menu_loop():
         textrect.center = ((display_width*0.5),(display_height*0.5))
         gameDisplay.blit(textsurf,textrect)
        
-        gameDisplay.blit(main_character,(display_width*0.4,display_height*(0.05)))
+        #gameDisplay.blit(main_character,(display_width*0.4,display_height*(0.05)))
 
         #rectan_button(msg,x,y,w,h,ic=green,ac=green_bright,action=None,size=20,font='freesansbold.ttf')
         rectan_button('Start',250,450,250,100,green,green_bright,pick_char)
